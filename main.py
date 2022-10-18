7f554a73810efe5dbeeb5c59387a19acd356a2ca
p-Cipriano <cipriano.pedrof@gmail.com>
2022-10-18 18:48:53 -0300

    Added search bar(front not updated yet)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, Blueprint
 import sqlite3
-import urllib.request, json
 import os
 import re
 from login_system import loginPage_BP
 from database.db_system import userInfo
+from bookSearch import bookSearch, SearchForm
 
 app = Flask(__name__)
 app.secret_key = "LIMAO"
@@ -21,20 +21,20 @@ def main():
 if __name__ == "__main__":
     main()
 
-@app.route('/home')
+@app.route('/home', methods=["POST", "GET"])
 def home():
     if 'loggedin' in session: # Aqui ta um exemplo de um request, é apenas um teste tem que mudar praticamente tudo, o primeiro passo provavel, é ver bem como dar o request e quais informações vao pegar, depois montar um digrama de classes de como vao quardar essas infos, tipo, classe Book, atributo titulo, capa, descrição. Metodo: getTitle() etc. 
-        # Get Books
-        titulo = "O Ardiloso Cortes".replace(" ", "%20")
-        url = "https://www.googleapis.com/books/v1/volumes?q=\"{}\"&orderBy=relevance&key=AIzaSyA6Z7tIcH7mawIpXKKUVrkVMNe4hD-uZNs".format(titulo)
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        data = json.loads(data)
+        form = SearchForm()
+        if form.validate_on_submit():
+            data = bookSearch(form.searched.data)
+            form.searched.data = ''
+        else:
+            data = bookSearch("O Ardiloso Cortes".replace(" ", "%20"))
         bookList = []
         for i in range(len(data["items"])):  
             if "imageLinks" in data["items"][i]["volumeInfo"]:
                 bookList.append([data["items"][i]["volumeInfo"]["title"], data["items"][i]["volumeInfo"]["imageLinks"]["smallThumbnail"]])
-        return render_template('home.html', username=session['username'], titles=bookList)
+        return render_template('home.html', form=form, username=session['username'], titles=bookList)
     return redirect(url_for('loginPage.login'))
 
 @app.route('/profile')
