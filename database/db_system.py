9db476d2594bbd56f69a49d70d7ce735c71646d3
DanielAugusto191 <daniel.augusto191@gmail.com>
2022-10-20 17:29:51 -0300

    database sigs

--- database/db_system.py
+++ database/db_system.py
@@ -1,7 +1,40 @@
 # Aqui vai ter request ao banco para profiles.
 import sqlite3
+"""
+Summary:
+
+userInfo(id) - Given a user id return yours info.
+updatePassword(id, oldPassword, newPassword) -  Update user's password
+updateProfilePicture(id, newProfilePicture) - Update user's profile picture
+updateUsername(id, newUserName) - Update user's username
+setReview(userID, bookID, review) - Set a new review of a book
+updateReview(userID, bookID, review) - Update a review of a book
+getReview(userID, bookID) - Get a User's review of a book
+getAllReviews(userID) - Get all reviews of a User
+setRate(userID, bookID, rate) - Assigns the rate the user has given to a book
+updateRate(userID, bookID, newRate) - Update the rate the user has given to a book
+toogleBookAsFavorite(UserID, bookID) - Set/Unset a book as users favorite. (Limit of 5 books)
+requestFavoritesBooks(UserID, bookID) - Return the users favorite books
+
+Parameters and Return are given below:
+"""
 
 def userInfo(id):
+    ''' 
+Given a user id return yours info.
+
+Parametes:
+id = User ID
+
+Return:
+tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Empty if works, or return the error
+    account = Dict with accout infos:
+        - ID = User id
+        - Username = User's username
+        - Email = user's Email
+'''
     works = False
     msg = ""
     account = ""
@@ -10,8 +43,180 @@ def userInfo(id):
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
         cur.execute('SELECT * FROM User WHERE id = ?', (id,))
-        account = cur.fetchone()
-        works = True
+        account = dict(cur.fetchone())
+        del account["password"]
     except Exception as e:
         msg = e
     return (works, msg, account)
+
+def updatePassword(id, oldPassword, newPassword):
+    ''' 
+Update user's password
+
+Parameters:
+id = User id
+oldPassword = String with the User's Old Password to confirm
+newPassword = String with the User's New Password to set as the new Password
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def updateProfilePicture(id, newProfilePicture):
+    ''' 
+Update user's profile picture
+
+Parameters:
+id = User id
+newProfilePicture = data for set new profile picture
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def updateUsername(id, newUserName):
+    ''' 
+Update user's username
+
+Parameters:
+id = User id
+newUserName = New username to update.
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def setReview(userID, bookID, review):
+    ''' 
+Set a new review of a book
+
+Parameters:
+userID = User id
+bookID = Book id
+Review = String with the user review.
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def updateReview(userID, bookID, review):
+    ''' 
+Update a review of a book
+
+Parameters:
+userID = User id
+bookID = Book id
+Review = String with the new user review.
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def getReview(userID, bookID):
+    ''' 
+Get a User's review of a book
+
+Parameters:
+userID = User id
+bookID = Book id
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+    review = Users review (that are Dicts of Books ID, Books name, rate and Review) - {"BookID"=ID, "BookName"=BooksName, "rate"=rate, "review"=Review}
+'''
+    pass
+
+def getAllReviews(userID):
+    ''' 
+Get all reviews of a User
+
+Parameters:
+userID = User id
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"error" - Status of works.
+    reviews = List of Reviews(that are Dicts of Books ID, Books name, rate and Review) - [{"BookID"=ID, "BookName"=BooksName,"rate"=rate, "review"=Review}, {"BookID"=ID, "BookName"=BooksName, "rate"=rate, "review"=Review}, ...]
+'''
+    pass
+
+def setRate(userID, bookID, rate):
+    ''' 
+Assigns the rate the user has given to a book
+
+Parameters:
+userID = User id
+bookID = Book id
+rate = The rate that user gives to the book.
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def updateRate(userID, bookID, newRate):
+    ''' 
+Update the rate the user has given to a book
+
+Parameters:
+userID = User id
+bookID = Book id
+newRate = The rate that user gives to the book.
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def toogleBookAsFavorite(UserID, bookID):
+    ''' 
+Set/Unset a book as users favorite. (Limit of 5 books)
+
+Parameters:
+userID = User id
+bookID = Book id
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+'''
+    pass
+
+def requestFavoritesBooks(UserID, bookID):
+    ''' 
+Return the users favorite books
+
+Parameters:
+userID = User id
+bookID = Book id
+
+Return:
+Tuple of:
+    works = True/False - If there is no error.
+    msg = ""/"{error}" - Status of works.
+    Books = List of Reviews(that are Dicts of Books ID, Books name, rate and Review) - [{"BookID"=ID, "BookName"=BooksName,"rate"=rate, "review"=Review}, {"BookID"=ID, "BookName"=BooksName, "rate"=rate, "review"=Review}, ...]
+'''
+    pass
