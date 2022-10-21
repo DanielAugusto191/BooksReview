e0ba808ffc4bebf4cfc814796d55be030c2b30c0
DanielAugusto191 <daniel.augusto191@gmail.com>
2022-10-21 10:33:23 -0300

    profile database functions

--- database/db_config.py
+++ database/db_config.py
@@ -1,7 +1,11 @@
+import db_system
 import sqlite3
 connection = sqlite3.connect("database/database.db")
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
+
+# db_system.updateUsername(1, "sasa")
+# db_system.setReview(1, "TESTE", "TESTE@")
 
 with open("database/db_config.sql") as f:
     connection.executescript(f.read())
