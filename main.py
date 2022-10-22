74648b706c692af760da2fd18249dc01316e761a
Kayke Campos <kaykerenan@hotmail.com>
2022-10-22 15:37:54 -0300

    Remoção do IF por causa do valor default

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def home():
                 if (("imageLinks" in data["items"][i]["volumeInfo"]) and ("description" in data["items"][i]["volumeInfo"])
                 and ("authors" in data["items"][i]["volumeInfo"])):
                     bookList.append(Book(data["items"][i]))
-        bookList = sortBookList(bookList, searchForm.sortBy.data)
+            bookList = sortBookList(bookList, searchForm.sortBy.data)
         return render_template('home.html', searchForm = searchForm, username=session['username'], titles=bookList)
     return redirect(url_for('loginPage.login'))
 
