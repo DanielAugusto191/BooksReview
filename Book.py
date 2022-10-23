5541196e9a59cc26f9c5599eafd3d08a36906b53
p-Cipriano <cipriano.pedrof@gmail.com>
2022-10-23 20:39:24 -0300

    Added author info for searches

--- Book.py
+++ Book.py
@@ -9,7 +9,9 @@ def sortBookList(bookList, sortBy='relevance'):
 class Book:
     def __init__(self, bookData):
         self.title = bookData['volumeInfo']['title']
-        self.authors = bookData['volumeInfo']['authors']
+        self.authors = bookData['volumeInfo']['authors'][0]
+        for i in range(1, len(bookData['volumeInfo']['authors'])):
+            self.authors = self.authors + ", " + bookData['volumeInfo']['authors'][i]
         self.imageLink = bookData['volumeInfo']['imageLinks']['smallThumbnail']
         self.description = bookData['volumeInfo']['description']
         self.id = bookData['id']
